045c2984fe0eeca634903ce8882de78f36497667
David <david.scott.2875@gmail.com~>
2025-01-13 16:26:32 +0100

    final

--- main.py
+++ main.py
@@ -22,17 +22,50 @@ with st.form('form', clear_on_submit=True):
             smoker = 0
     option2 = st.selectbox(
         "number of kids",
-        ("","0","1-4","more than 5"),)
+        ("","0","1","2","3","4","more than 5"),)
     if option2 != "":
         if option2 == "0":
             nokid = 1
-            kids = 0
-        if option2 == "1-4":
+            onekid = 0
+            twokid = 0
+            threekid = 0
+            fourkid = 0
+            fivekid = 0
+        if option2 == "1":
             nokid = 0
-            kids = 1
+            onekid = 1
+            twokid = 0
+            threekid = 0
+            fourkid = 0
+            fivekid = 0
+        if option2 == "2":
+            nokid = 0
+            onekid = 0
+            twokid = 1
+            threekid = 0
+            fourkid = 0
+            fivekid = 0
+        if option2 == "3":
+            nokid = 0
+            onekid = 0
+            twokid = 0
+            threekid = 1
+            fourkid = 0
+            fivekid = 0
+        if option2 == "4":
+            nokid = 0
+            onekid = 0
+            twokid = 0
+            threekid = 0
+            fourkid = 1
+            fivekid = 0
         if option2 == "more than 5":
             nokid = 0
-            kids = 0
+            onekid = 0
+            twokid = 0
+            threekid = 0
+            fourkid = 0
+            fivekid = 1
     option3 = st.selectbox(
     "region",
     ("","northeast","southeast","southwest", "northwest"),)
@@ -51,7 +84,11 @@ with st.form('form', clear_on_submit=True):
             is_east = 0
     option4 = st.selectbox(
     "sex",
-    ("","male","female"),)   
+    ("","male","female"),)
+    if option4 == "male":
+        sex = 1
+    if option4 == "female":
+        sex = 0
     bmi = st.number_input(label="enter bmi")
     age = st.number_input("enter age", min_value=0, max_value=90,step=1)
     if bmi > 45.5:
@@ -60,35 +97,79 @@ with st.form('form', clear_on_submit=True):
         overweight = 0
         severlyoverweight = 0
         ogre = 1
-    if bmi < 17:
+    if bmi < 17.85:
         underweight = 1
         normal = 0
         overweight = 0
         severlyoverweight = 0
         ogre = 0
-    if bmi > 38:
-        uunderweight = 1
-        normal = 0
-        overweight = 0
-        severlyoverweight = 0
-        ogre = 0
-    if bmi >= 18 and bmi <= 38:
+    if bmi >= 17.85 and bmi < 30.5:
         underweight = 1
         normal = 0
         overweight = 0
         severlyoverweight = 0
         ogre = 0
-    if age != 0:
-        if age > 60 and option3 == "female":
-            elderfemale = 1
-        else:
-            elderfemale = 0
+    if bmi >= 30.5 and bmi <= 37.5:
+        underweight = 0
+        normal = 0
+        overweight = 1
+        severlyoverweight = 0
+        ogre = 0
+    if bmi >= 37.5 and bmi <= 45.5:
+        underweight = 0
+        normal = 0
+        overweight = 0
+        severlyoverweight = 1
+        ogre = 0
+    if age > 60.75:
+        teens = 0
+        test = 0
+        youngfemale = 0
+        youngerfemale = 0
+        middleagefemale = 0
+        elderfemale = 1
+    if age > 46.5 and age <= 60.75:
+        teens = 0
+        test = 0
+        youngfemale = 0
+        youngerfemale = 0
+        middleagefemale = 1
+        elderfemale = 0
+    if age > 42.5 and age <= 46.5:
+        teens = 0
+        test = 0
+        youngfemale = 0
+        youngrfemale = 1
+        middleagefemale = 0
+        elderfemale = 0
+    if age < 21.75:
+        teens = 1
+        test = 0
+        youngfemale = 0
+        youngerfemale = 0
+        middleagefemale = 0
+        elderfemale = 0
+    if age > 21.5 and age <= 31:
+        teens = 0
+        test = 1
+        youngfemale = 0
+        youngerfemale = 0
+        middleagefemale = 0
+        elderfemale = 0
+    if age > 31 and age <= 39.5:
+        teens = 0
+        test = 0
+        youngfemale = 0
+        youngerfemale = 1
+        middleagefemale = 0
+        elderfemale = 0
+
 
     if st.form_submit_button("submit") :
         X = list(('smoker','normal', 'is_north','underweight', 'overweight','severlyoverweight','ogre','nokid', 'onekid', 'twokid','fourkid', 'fivekid','test','elderfemale','middleagefemale', 'youngerfemale', 'youngfemale', 'teens', 'sex'))
         with open('model.pkl', 'rb') as f:
             model = pickle.load(f)
-        X= np.asarray([smoker, ])
+        X= np.asarray([smoker, normal, is_north, underweight, overweight, severlyoverweight, ogre, nokid, onekid, twokid,fourkid, fivekid,test,elderfemale,middleagefemale, youngerfemale, youngfemale, teens, sex])
         X=X.reshape(1,-1)
         poly = PolynomialFeatures(degree=2)
         poly_variables = poly.fit_transform(X)
